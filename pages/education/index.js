e9a10e95443de77d170761ae841c9aebc2dd2799
harshit <harshitgangwar51102@gmail.com>
2025-01-05 13:31:00 +0530

    adding gsap to education page

--- pages/education/index.js
+++ pages/education/index.js
@@ -3,7 +3,7 @@ import Head from "next/head";
 import Navbar from "../../components/Navbar";
 import grm_school from "../../public/grm_school.png"
 import iiitdmj from "../../public/iiitdmj.png"
-
+import gsap from "gsap";
 import Image from "next/image";
 
 
@@ -20,6 +20,30 @@ function Education() {
         setDarkMode(newDarkMode);
         localStorage.setItem("darkMode", JSON.stringify(newDarkMode));
     };
+
+
+    useEffect(() => {
+        const ctx = gsap.context(() => {
+          gsap.from(".edu1", {
+            x:"-100%",
+            opacity: 0, 
+            duration:1
+          });
+          gsap.from(".edu2", {
+            x:"100%",
+            opacity: 0, 
+            duration:1
+          });
+          gsap.from(".edu3", {
+            x:"-100%",
+            opacity: 0, 
+            duration:1
+          });
+    
+        });
+          return () => ctx.revert();
+      }, []);
+
     return (
         <div className={`${darkMode ? "dark" : ""}`}>
             <Head>
@@ -33,7 +57,7 @@ function Education() {
                     <div className="text-center md:px-40 mb-20">
                         <h3 className="text-5xl font-bold py-1 dark:text-white ">EDUCATION</h3>
                     </div>
-                    <div className=" w-full border-2 border-sky-500 rounded-md shadow-lg shadow-indigo-500/50 dark:text-white p-4 grid-cols-1 md:grid grid-cols-3 gap-4 mb-8">
+                    <div className="edu1 w-full border-2 border-sky-500 rounded-md shadow-lg shadow-indigo-500/50 dark:text-white p-4 grid-cols-1 md:grid grid-cols-3 gap-4 mb-8">
                         <div className="col-span-2 text-center flex  items-center">
                             <div>
                                 <h1 className="text-3xl mb-4">Undergraduate Education</h1>
@@ -42,7 +66,7 @@ function Education() {
                         </div>
                         <div><Image src={iiitdmj} className="rounded-xl shadow-lg md:-mt-12 md:ml-12" alt="img"/></div>
                     </div>
-                    <div className=" w-full border-2 border-sky-500 rounded-md shadow-lg shadow-indigo-500/50 dark:text-white p-4 grid-cols-1 md:grid grid-cols-3 gap-4 mb-8">
+                    <div className="edu2 w-full border-2 border-sky-500 rounded-md shadow-lg shadow-indigo-500/50 dark:text-white p-4 grid-cols-1 md:grid grid-cols-3 gap-4 mb-8">
                         <div><Image src={grm_school} className="rounded-xl shadow-lg md:-mb-8 md:-ml-12" alt="img"/></div>
                         <div className="col-span-2 text-center flex items-center ">
                             <div>
@@ -51,7 +75,7 @@ function Education() {
                             </div>
                         </div>
                     </div>
-                    <div className=" w-full border-2 border-sky-500 rounded-md shadow-lg shadow-indigo-500/50 dark:text-white p-4 grid-cols-1 md:grid grid-cols-3 gap-4 mb-8">
+                    <div className="edu3 w-full border-2 border-sky-500 rounded-md shadow-lg shadow-indigo-500/50 dark:text-white p-4 grid-cols-1 md:grid grid-cols-3 gap-4 mb-8">
                         <div className="col-span-2 text-center flex items-center ">
                             <div>
                                 <h1 className="text-3xl mb-4">Secondary Education</h1>
