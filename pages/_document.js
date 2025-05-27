875ec17c502199f0d09eb98e2325851ea5677933
harshit <harshitgangwar51102@gmail.com>
2025-05-27 12:26:20 +0530

    adding google meta tag

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <meta name="google-site-verification" content="n8jGq41ll19jctWXMhi0cz3VqppK-Xlna7qz7ufnQ9k" />
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap"
           rel="stylesheet"
